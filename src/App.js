deb1651924b96004204a103be5907c69066d6edb
Devang Mathur <masterdevang@gmail.com>
2021-08-13 15:25:38 +0530

    use state included

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+
 import './App.css';
 
+const db = {
+  JavaScript: [
+    {
+      name: "Eloquent JavaScript",
+      rating: "4/5",
+      author: "By-: Marjin Haverbeke"
+    },
+    {
+      name: "You Don't Know JS",
+      rating: "3.5/5",
+      author: "By-: Kyle Simpson"
+    },
+  ],
+  GeneralComputer: [
+    {
+      name: "Nand2Tetris",
+      rating: "4.5/5",
+      author: "By-: Noam Nisan(MIT Press)",
+    },
+  ],
+  fiction: [
+    {
+      name: "Shiva Trilogy",
+      rating: "5/5",
+      author: "By-: Amish Tripathi"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "4.5/5",
+      author: "By-: J.K. Rowling",
+    },
+  ],
+}
+
+
+
 function App() {
+  const [selectedGenre, setGenre] = useState("GeneralComputer")
+  function clickHandler(genre) {
+    setGenre(genre)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Good Books📚📚📖📕</h1>
+
+      <div>
+        {Object.keys(db).map((genre) => (
+          <button
+            style={{
+              padding: "0.5rem 1rem",
+              margin: "0.5rem 0.5rem",
+              border: "1px solid black",
+              borderRadius: "0.5rem",
+              backgroundColor: "#6EE7B7",
+              cursor: "pointer"
+
+            }}
+            onClick={() => clickHandler(genre)}>
+            {genre}
+          </button>
+        ))
+        }
+      </div>
+
+      <div>
+        <ul>
+          {db[selectedGenre].map((book) => (
+            <div style={{
+              listStyle: "none",
+              padding: "1rem",
+              border: "1px solid #D1D5DB",
+              width: "80%",
+              margin: "1rem 5rem",
+              borderRadius: "0.5rem",
+              textAlign: "center"
+            }}>
+              <li style={{ fontSize: "larger", textAlign: 'center' }}>{book.name}</li>
+              <li style={{ fontSize: "smaller", textAlign: 'center' }}>{book.rating}</li>
+              <li style={{ fontSize: "smaller", textAlign: 'center' }}>{book.author}</li>
+            </div>
+          ))}
+        </ul>
+
+      </div>
+
     </div>
   );
 }
